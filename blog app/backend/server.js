5e0b809847d23fb593618869fc867718f08049fc
HamzaUetm <Hamzaxyz2315@gmail.com>
2025-05-17 10:00:39 -0700

    Add files via upload

---
+++ blog app/backend/server.js
@@ -0,0 +1,22 @@
+
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+require('dotenv').config();
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+const postRoutes = require('./routes/posts');
+app.use('/posts', postRoutes);
+
+const PORT = process.env.PORT || 5000;
+
+mongoose.connect(process.env.MONGO_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+}).then(() => {
+  console.log('MongoDB connected');
+  app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+}).catch(err => console.error(err));
