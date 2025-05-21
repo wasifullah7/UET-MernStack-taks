a350201b7517f2832ea7dd5cf1f6f70a57282d43
Umair khan <152378419+assistantsari@users.noreply.github.com>
2025-05-21 10:57:32 -0700

    Create commit.js

---
+++ blog-app/blog-backend/models/commit.js
@@ -0,0 +1,8 @@
+const mongoose = require('mongoose');
+
+const commentSchema = new mongoose.Schema({
+  postId: { type: mongoose.Schema.Types.ObjectId, ref: 'Post', required: true },
+  text: { type: String, required: true },
+}, { timestamps: true });
+
+module.exports = mongoose.model('Comment', commentSchema);
